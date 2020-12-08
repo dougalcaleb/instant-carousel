bd090976c12a5e44e015d86ace6aa269d68f6f2b
dougalcaleb <dougalcaleb@gmail.com>
2020-12-08 07:48:28 -0700

    Add template settings file

---
+++ javascript/settings.js
@@ -0,0 +1,14 @@
+new Roundabout({
+   id: "#carouselOne",
+   pages: [
+      {
+         background_image: "../images/numbers/1.png"
+      },
+      {
+         background_image: "../images/numbers/2.png"
+      },
+      {
+         background_image: "../images/numbers/3.png"
+      }
+   ]
+});
