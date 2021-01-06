3112fc977a8f636392883757ccabb2036a94bda0
dougalcaleb <dougalcaleb@gmail.com>
2021-01-06 16:11:06 -0700

    Tracking this file now. Will remove from releases

---
+++ javascript/carousel-settings.js
@@ -0,0 +1,174 @@
+let c1 = new Roundabout({
+	pagesToShow: 6,
+	pageSpacing: 20,
+	spacingMode: "fill",
+   transition: 600,
+   infinite: false,
+   scrollBy: 4,
+   swipe_resistance: 0.5,
+   pages: [
+      {
+         background_image: "../images/numbers/0.png",
+      },
+		{
+			background_image: "../images/numbers/1.png",
+		},
+		{
+			background_image: "../images/numbers/2.png",
+		},
+		{
+			background_image: "../images/numbers/3.png",
+		},
+		{
+			background_image: "../images/numbers/4.png",
+		},
+		{
+			background_image: "../images/numbers/5.png",
+		},
+		{
+			background_image: "../images/numbers/6.png",
+      },
+      {
+			background_image: "../images/numbers/7.png",
+      },
+      {
+			background_image: "../images/numbers/8.png",
+      },
+      {
+			background_image: "../images/numbers/9.png",
+		},
+
+
+		// {
+		// 	background_image: "./images/numbers/1.png",
+		// },
+		// {
+		// 	background_image: "../images/numbers/2.png",
+		// },
+		// {
+		// 	background_image: "../images/numbers/3.png",
+		// },
+		// {
+		// 	background_image: "../images/numbers/4.png",
+		// },
+		// {
+		// 	background_image: "../images/numbers/5.png",
+		// },
+		// {
+		// 	background_image: "../images/numbers/6.png",
+		// },
+		// {
+		// 	background_image: "./images/numbers/1.png",
+		// },
+		// {
+		// 	background_image: "../images/numbers/2.png",
+		// },
+		// {
+		// 	background_image: "../images/numbers/3.png",
+		// },
+		// {
+		// 	background_image: "../images/numbers/4.png",
+		// },
+		// {
+		// 	background_image: "../images/numbers/5.png",
+		// },
+		// {
+		// 	background_image: "../images/numbers/6.png",
+		// },
+	],
+});
+
+// let c2 = new Roundabout({
+// 	showPages: 2,
+//    swipe_multiplier: 1,
+//    transition: 500,
+//    // infinite: false,
+//    id: ".carousel2",
+//    scrollBy: 2,
+// 	pages: [
+// 		{
+// 			background_image: "./images/numbers/1.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/2.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/3.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/4.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/5.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/6.png",
+// 		},
+
+// 		{
+// 			background_image: "./images/numbers/1.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/2.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/3.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/4.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/5.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/6.png",
+// 		},
+// 		{
+// 			background_image: "./images/numbers/1.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/2.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/3.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/4.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/5.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/6.png",
+// 		},
+// 	],
+// });
+
+
+// new Roundabout({
+// 	showPages: 2,
+//    swipe_multiplier: 1,
+//    transition: 200,
+//    infinite: false,
+//    id: ".carousel3",
+// 	pages: [
+// 		{
+// 			background_image: "./images/numbers/1.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/2.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/3.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/4.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/5.png",
+// 		},
+// 		{
+// 			background_image: "../images/numbers/6.png",
+// 		},
+// 	],
+// });
