b9b330d07b6c0b9719bdc763419db75226c38e89
dougalcaleb <dougalcaleb@gmail.com>
2021-03-16 07:34:55 -0600

    tweaks

--- src/dev-settings.js
+++ src/dev-settings.js
@@ -1,8 +1,14 @@
 const rs = new RoundaboutScripter();
 const c = new Roundabout({
-   pagesToShow: 5,
+
+   type: "gallery", // normal should be slider
+
+   pagesToShow: 3,
    scrollBy: 3,
-   transition: 500,
+   transition: 5000,
+
+   navigation: false,
+   transitionFunction: "ease-in-out",
    // pageSpacing: 10,
    // pageSpacingUnits: "px",
 
@@ -23,8 +29,8 @@ const c = new Roundabout({
    pages: [
       {
          backgroundImage: "../images/numbers/0.png",
-         html: "<a href='https://github.com/dougalcaleb/roundabout' target='_blank'><button class='toRepo'>Go to Roundabout repo</button></a>",
-         css: ".toRepo {position: absolute; left: 0; right: 0; margin: auto; top: 30px; z-index: 3; border: 3px solid white; background: none; color: white; outline: none; cursor: pointer;}"
+         // html: "<a href='https://github.com/dougalcaleb/roundabout' target='_blank'><button class='toRepo'>Go to Roundabout repo</button></a>",
+         // css: ".toRepo {position: absolute; left: 0; right: 0; margin: auto; top: 30px; z-index: 3; border: 3px solid white; background: none; color: white; outline: none; cursor: pointer;}"
       },
       {
          backgroundImage: "../images/numbers/1.png"
@@ -74,32 +80,32 @@ const c = new Roundabout({
    ]
 });
 
-rs.onScroll(c, () => {
-   console.log("Scroll");
-});
+// rs.onScroll(c, () => {
+//    console.log("Scroll");
+// });
 
-rs.onScrollEnd(c, () => {
-   console.log("Scroll ended");
-});
+// rs.onScrollEnd(c, () => {
+//    console.log("Scroll ended");
+// });
 
-rs.onDragStart(c, () => {
-   console.log("Drag start");
-});
+// rs.onDragStart(c, () => {
+//    console.log("Drag start");
+// });
 
-rs.onDragEnd(c, () => {
-   console.log("Drag ended");
-});
+// rs.onDragEnd(c, () => {
+//    console.log("Drag ended");
+// });
 
-rs.onScrollNext(c, () => {
-   console.log("Scrolled Next");
-});
+// rs.onScrollNext(c, () => {
+//    console.log("Scrolled Next");
+// });
 
-rs.onScrollPrevious(c, () => {
-   console.log("Scrolled Previous");
-});
+// rs.onScrollPrevious(c, () => {
+//    console.log("Scrolled Previous");
+// });
 
-function set() {
-   rs.setValue(c, "pagesToShow", 2);
-   rs.setValue(c, "scrollBy", 2);
-   rs.destroy(c);
-}
+// function set() {
+//    rs.setValue(c, "pagesToShow", 2);
+//    rs.setValue(c, "scrollBy", 2);
+//    rs.destroy(c);
+// }
